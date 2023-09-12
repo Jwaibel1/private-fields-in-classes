f9b5e1cedb1919e79ed3041774c600d60fc04685
Jwaibel1 <joshuawaibel8@gmail.com>
2023-09-12 13:49:35 -0500

    Class Session 6 - Exercise 6

--- person.js
+++ person.js
@@ -1,5 +1,13 @@
 class Person {
-  // Write code here
+  #id;
+
+  constructor(id) {
+    this.#id = id;
+  }
+
+  get() {
+    return Math.floor(Math.random() * 1000);
+  }
 }
 
 module.exports = Person;
